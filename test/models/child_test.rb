ca55b6920af8b062e29c2e9291ebdade54bde760
Blythe Weng <bweng@andrew.cmu.edu>
2021-02-04 13:49:35 -0500

    100% coverage

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -37,6 +37,13 @@ class ChildTest < ActiveSupport::TestCase
      assert_equal ["Alex", "Mark", "Rachel"], Child.alphabetical.map{|c| c.first_name}
    end
 
+   should "show the correct points earned by children" do 
+   	assert_equal 4, @alex.points_earned
+   	assert_equal 1, @mark.points_earned
+   	assert_equal 0, @rachel.points_earned
+
+   end
+
    should "have a scope to select only active children" do
      assert_equal ["Alex", "Mark"], Child.active.alphabetical.map{|c| c.first_name}
    end
