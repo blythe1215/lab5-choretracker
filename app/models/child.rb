b0af98811c0ec9b74819461a8f4e43c89275f532
Blythe Weng <bweng@andrew.cmu.edu>
2021-02-04 12:59:05 -0500

    fixed errors

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,24 @@
 class Child < ApplicationRecord
+
+#Relationships
+
+has_many :chores
+has_many :tasks, through: :chores
+
+
+#Validations
+
+validates_presence_of :first_name
+validates_presence_of :last_name
+
+#Scopes
+scope :alphabetical, -> { order('last_name, first_name') }
+scope :active, -> { where(active: true) }
+
+#Methods
+
+def name
+	return first_name + " " + last_name
+end
     
 end
